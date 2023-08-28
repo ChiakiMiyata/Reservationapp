ed1329a6aa7e0b1eddf90c1e4efe701cdb484a3a
ChiakiMiyata <mycaymkz15@gmail.com>
2023-08-28 06:23:21 +0000

    add ユーザー登録/編集機能、ログイン/ログアウト機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/top'
+  devise_for :users, controllers: { registrations: 'users/registrations',
+                                   sessions: 'users/sessions' }
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
